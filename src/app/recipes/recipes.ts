33da2fb25b88a008b578cc21c5f726e33489606b
Kasey Labelle <kasey.l.labelle@gmail.com>
2020-04-12 18:58:38 -0400

    add recipes, fix shadows

--- src/app/recipes/recipes.ts
+++ src/app/recipes/recipes.ts
@@ -1,5 +1,84 @@
 export const recipes = [
   {
+    name: "Apple Pie Filling",
+    ingredients: [
+      "5 green apples, peeled & sliced",
+      "1 cup sugar",
+      "2 tbsp flour",
+      "1/2 tsp cinnamon",
+      "1/4 tsp salt",
+      "1 tbsp cider vinegar",
+      "1 tbsp lemon juice"
+    ],
+    instructions: [
+      "Peel and slice apples",
+      "Soak apples in water and lemon juice",
+      "Mix sugar, flour, cinnamon, salt, and cider vinegar",
+      "Drain apples, and mix together",
+    ]
+  },
+  {
+    name: "Apple Pumpkin Muffins",
+    ingredients: [
+      "1 1/2 cup whole wheat flour",
+      "1/2 cup packed brown sugar",
+      "1 tsp pumpkin pie spice",
+      "3/4 tsp baking powder",
+      "1/2 tsp baking soda",
+      "1/2 tsp salt",
+      "2 large eggs",
+      "3/4 cup pumpkin purée",
+      "2/3 cup cinnamon applesauce",
+      "1/2 cup vegetable oil",
+      "1 tbsp butter, melted",
+      "1/3 cup powdered sugar",
+      "cinnamon to taste"
+    ],
+    instructions: [
+      "Preheat oven to 350° F",
+      "Prepare a muffin pan with paper liners or non-stick spray",
+      "Mix flour, brown sugar, pumpkin pie spice, baking powder, baking soda, and salt",
+      "Make a well in the center, and add eggs, pumpkin purée, applesauce, and oil, and stir until blended",
+      "Fill muffin cups to top",
+      "Bake ~20-22 minutes, or until a toothpick comes out dry",
+      "In a small bowl, mix melted butter, powdered sugar, cinnamon to taste, and water to icing consistency",
+      "Drizzle icing mixture over muffins"
+    ]
+  },
+  {
+    name: "Buddha Bowl",
+    ingredients: [
+      "1 bag broccoli, chopped",
+      "1 bag cauliflower, chopped",
+      "1 red onion, chopped",
+      "2-3 sweet potatoes, chopped",
+      "1 bag kale",
+      "salt & pepper to taste",
+      "garlic powder",
+      "tahini",
+      "lemon juice",
+      "lemon pepper grilled chicken",
+      "1 cup quinoa, dry"
+    ],
+    instructions: [
+      "In a large bowl, add chicken",
+      "Brush chicken with olive oil, rub with lemon pepper spice mixture, and add lemon juice",
+      "Marinate chicken for at least 30 minutes",
+      "On a baking sheet, drizzle broccoli, cauliflower, and red onion with olive oil and season with salt and pepper",
+      "On a separate baking sheet, drizzle sweet potato with olive oil and season with salt and peper",
+      "Roast at 375° F until cooked, flipping every ~10-15 minutes",
+      "Heat cast iron grill pan",
+      "Add chicken, cook undisturbed for 5-7 minutes",
+      "Flip chicken, cook undisturbed for 5-7 minutes",
+      "Remove from heat, let rest 5 minutes, and cut into bites",
+      "Cook quinoa according to instructions",
+      "In a small bowl, mix tahini, garlic powder, and lemon juice",
+      "Slowly add water until the mixture becomes the consistency of dressing",
+      "In a pan, sauté kale with olive oil, garlic powder, and salt",
+      "Serve in a bowl, and drizzle with garlicky lemony tahini dressing"
+    ]
+  },
+  {
     name: "Chicken & Quinoa Soup",
     ingredients: [
       "3 tbsp olive oil",
@@ -7,7 +86,7 @@ export const recipes = [
       "3 stalks celery, chopped",
       "2 cups carrots, chopped",
       "1 tsp cajun seasoning",
-      "1 cup quinoa",
+      "1 cup quinoa, dry",
       "32 oz low sodium chicken broth",
       "3 cups water",
       "2 1/2 cups shredded chicken breast",
@@ -16,16 +95,61 @@ export const recipes = [
     instructions: [
       "Heat 2 tbsp olive oil in a large pot",
       "Add onion, celery, carrots, and cajun seasoning",
-      "Cook ~5 minutes, until slightly soft",
+      "Cook ~5 minutes until slightly soft",
       "Add quinoa, broth, water, salt, and pepper",
       "Partially cover and bring to a simmer",
-      "Reduce to medium heat and cook ~12 minutes, until quinoa softens",
+      "Reduce to medium heat and cook ~12 minutes until quinoa softens",
       "In a pan, sauté chicken in 1 tbsp olive oil with cajun seasoning",
       "Remove chicken from pan and pull",
       "Add chicken and broccoli to pot, and cook ~5 minutes"
+    ]
+  },
+  {
+    name: "Chipotle Lime Blackened Chicken",
+    ingredients: [
+      "2 tbsp olive oil",
+      "1 tsp chipotle chili powder",
+      "1/2 tsp chili powder",
+      "1/2 tsp cumin",
+      "1/2 tsp smoked paprika",
+      "1/2 tsp onion powder",
+      "1/2 tsp garlic powder",
+      "1/2 tsp salt",
+      "1/4 tsp pepper",
+      "1 tbsp lime juice",
+      "6 thinly sliced chicken breasts"
     ],
-    notes: [
-      
+    instructions: [
+      "Mix spices in a small bowl or a spice jar",
+      "In a large bowl, add chicken",
+      "Brush chicken with olive oil, rub with spice mixture, and add lime juice",
+      "Marinate chicken for at least 30 minutes or up to 8 hours",
+      "Heat skillet",
+      "Add chicken, cook undisturbed for 3-5 minutes",
+      "Flip chicken, cook undisturbed for 5-7 minutes",
+      "Remove from heat, let rest 5 minutes",
+      "Cut into strips and serve over Mexican Quinoa"
+    ]
+  },
+  {
+    name: "Crispy Smashed Potatoes",
+    ingredients: [
+      "baby red potatoes",
+      "olive oil",
+      "garlic powder",
+      "rosemary",
+      "salt & pepper to taste"
+    ],
+    instructions: [
+      "Preheat oven to 500° F",
+      "Fill bottom of a pan with water",
+      "Place potatoes in pan, cover with foil, bake 30 minutes",
+      "Remove foil, drain, cool 10 minutes",
+      "Toss potatoes with oil",
+      `Place potatoes on pan and "smash" to 1/2" thick using the bottom of a glass`,
+      "Drizzle potatoes with olive oil, sprinkle with garlic powder, rosemary, salt, and pepper",
+      "Bake at 500° F on the top rack for 15 minutes",
+      "Move to the lower rack, and bake for 30 minutes"
     ]
   },
   {
@@ -53,11 +177,48 @@ export const recipes = [
       "Place potatoes and tomatoes in a single layer onto the baking sheet, drizzle with olive oil, and season with salt and pepper",
       "Place chicken in a single layer around potatoes and tomatoes",
       "Pour the remaining marinade over the baking sheet",
-      "Roast ~25-30 minutes, until chicken is cooked",
+      "Roast ~25-30 minutes until chicken is cooked",
       "Add asparagus to the baking sheet for the last ~10 minutes of cooking time"
+    ]
+  },
+  {
+    name: "Honey Garlic Stir Fry",
+    ingredients: [
+      `3 chicken breasts, cut into 1" cubes`,
+      "1/2 cup brown rice, dry",
+      "2 cups broccoli, chopped",
+      "2 cups cauliflower, chopped",
+      "1 cup carrots, chopped",
+      "1/2 cup low sodium soy sauce",
+      "3 tbsp honey",
+      "3-4 cloves garlic, minced"
     ],
-    notes: [
-      
+    instructions: [
+      "Cook brown rice according to instructions",
+      "In a large pan, heat 1 tbsp oil over medium heat",
+      "Add vegetables, cook until tender", 
+      "Remove vegetables from pan and cover",
+      "Wipe pan and turn heat to high",
+      "Add 1 tbsp oil and chicken, and sprinkle with salt and pepper",
+      "Cook chicken 3-4 minutes on each side until golden brown",
+      "Add garlic, cook 30 seconds",
+      "Add vegetables, cook 2 minutes",
+      "Add soy sauce and honey, cook 30 seconds",
+      "Serve over brown rice"
+    ]
+  },
+  {
+    name: "Honey Roasted Carrots",
+    ingredients: [
+      "8 carrots, peeled & halved",
+      "3 tbsp olive oil",
+      "1/4 cup honey",
+      "salt & pepper to taste"
+    ],
+    instructions: [
+      "Preheat oven to 350° F",
+      "Place carrots in a bowl, coat with oil, toss with honey, and season with salt and pepper",
+      "Bake ~40-60 minutes until tender"
     ]
   },
   {
@@ -77,9 +238,59 @@ export const recipes = [
       "Let cool until room temperature",
       "Refrigerate (up to 7 days)",
       "Serve 12oz chai concentrate and 4oz 1% milk over ice"
+    ]
+  },
+  {
+    name: "Mama B's Chili",
+    ingredients: [
+      "1 zucchini, diced",
+      "1 bell pepper, diced",
+      "1 onion, diced",
+      "3 stalks celery, chopped",
+      "1 jalapeño, seeded & diced",
+      "2 cloves garlic, diced",
+      "1 small can tomato sauce",
+      "1 medium can diced tomatoes",
+      "1/2 lb ground beef or ground turkey",
+      "1/2 tsp crushed red pepper",
+      "1/2 tbsp oregano",
+      "1/4 tsp cayenne pepper",
+      "1/2 tbsp cumin",
+      "1/2 tsp salt",
+      "1/8 tsp pepper",
+      "1/2 tbsp chili powder",
+      "1/2 tbsp smoked paprika",
     ],
-    notes: [
-      
+    instructions: [
+      "In a large pot, sauté zucchini, bell pepper, onion, celery, jalapeño, and garlic until onions are cooked",
+      "Add crushed red pepper, oregano, cayenne pepper, and cumin",
+      "Add diced tomatoes and tomato sauce, and simmer",
+      "In a pan, cook ground beef or ground turkey",
+      "When cooked through, drain meat and add to pot",
+      "Add salt, pepper, chili powder, and cooked paprika, and simmer",
+      "Serve with tortilla chips"
+    ]
+  },
+  {
+    name: "Mexican Quinoa",
+    ingredients: [
+      "1 tbsp olive oil",
+      "3 bell peppers",
+      "1 small yellow onion, chopped",
+      "1 jalapeño, seeded & diced",
+      "2 cloves garlic, minced",
+      "1 cup quinoa, dry",
+      "10 oz can RO*TEL original no salt added diced tomatoes & green chilies",
+      "1 package low sodium taco seasoning",
+      "2 cups low sodium chicken broth"
+    ],
+    instructions: [
+      "Heat 2 tbsp olive oil in a large pan",
+      "Add bell peppers and onion, cook until slightly soft",
+      "Add jalapeño and garlic, cook until garlic is fragrant",
+      "Add quinoa, diced tomatoes & green chilies, taco seasoning, and chicken broth, and bring to a boil",
+      "Reduce heat to medium/low, and simmer ~15-20 minutes or until liquid is absorbed",
+      "Serve with chipotle lime blackened chicken"
     ]
   },
   {
@@ -98,9 +309,71 @@ export const recipes = [
       "Move mixture to pan",
       "Wet hands, shake off excess water, and press mixture into pan",
       "Let someone special lick the spoon"
+    ]
+  },
+  {
+    name: "Panko Chicken",
+    ingredients: [
+      "3 chicken breasts or 6 chicken tenders",
+      "1 egg",
+      "Japanese-style panko bread crumbs",
+      "rosemary",
+      "salt & pepper to taste",
+      "Saratoga spicy garlic sea salt",
+      "Saratoga chipotle olive oil"
     ],
-    notes: [
-      
+    instructions: [
+      "Preheat oven to 375° F",
+      "In a bowl, whisk egg",
+      "Coat chicken in egg, and then coat chicken in breadcrumbs",
+      "Place breaded chicken in glass baking dish",
+      "Drizzle breaded chicken with chipotle olive oil, sprinkle with spicy garlic sea salt, rosemary, salt, and pepper",
+      "Bake until chicken is cooked"
+    ]
+  },
+  {
+    name: "Pasta",
+    ingredients: [
+      "1/2 can Bertolli vodka sauce",
+      "1 bag spinach",
+      "1 zucchini",
+      "1 summer squash",
+      "2-3 bell peppers",
+      "1 lb ground turkey",
+      "6-8 oz brown rice penne",
+      "crushed red pepper",
+      "smoked paprika",
+      "chili powder",
+      "Saratoga chipotle olive oil"
+    ],
+    instructions: [
+      "In a large pan, heat olive oil",
+      "Add zucchini, summer squash, and bell peppers",
+      "Add crushed red pepper, smoked paprika, and chili powder",
+      "Cook until vegetables are soft",
+      "In a pan, cook ground turkey",
+      "While ground turkey is cooking, add vodka sauce and spinach to the large pan",
+      "When ground turkey is cooked, add to the large pan",
+      "Simmer, then serve"
+    ]
+  },
+  {
+    name: "Tacos",
+    ingredients: [
+      "roma tomatoes, chopped and seeded",
+      "1 bag spinach",
+      "1 avocado",
+      "1 lb ground beef or ground turkey",
+      "1 package taco seasoning",
+      "1 tbsp sour cream",
+      "1/2 tsp Tapatío hot sauce",
+      "flour tortillas or taco shells",
+    ],
+    instructions: [
+      "Prepare taco meat according to the directions on the seasoning package",
+      "Prep tomatoes, spinach, and avocado",
+      "In a small bowl, mix sour cream, Tapatío, and water to crema consistency",
+      "Spray a small pan with oil, heat pan, and cook flour tortillas"
     ]
   }
 ];
